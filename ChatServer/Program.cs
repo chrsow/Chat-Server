d1d2d63c6ab39a54e4ef06b2271090dfd4c81549
Peerawut Luengruengroj <pee.luang45@gmail.com>
2017-04-23 09:46:04 +0700

    Add Command Console

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -3,22 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatServer
 {
     class Program
     {
+        private IPAddress ip;
+        private TcpListener tcpList;
         static void Main(string[] args)
         {
-            
+            showCommandConsole();
         }
-        /*
-        static void startServer(){}    
-        static void stopServer(){}
-        static void login(string username, string password){}       
-        static List<Message> getUnreadMessage(string clientID, time lastMessage){}       
-        static void sentMessage(Message message){}
-        static Message getMessage(){}
-        */
+        static void startServer()
+        {
+            Console.WriteLine("Starting Server...");                                 
+        }
+        static void stopServer()
+        {
+            Console.WriteLine("Stopping Server...");
+        }
+        static void showCommandConsole()
+        {
+            string command;
+            Console.WriteLine("Chat Server console.\n\nPlease type command.\n");
+            Console.WriteLine("\t[start]\tStart Chat Server\n\t[stop]\tStop Chat Server");
+            Console.Write("\nCommand >> ");
+            command = Console.ReadLine();
+            switch(command)
+                {
+                    case "start" : startServer();
+                    break;
+                    case "stop" : stopServer();
+                    break;
+                    default : Console.Out.WriteLine("Invalid command");
+                    break;
+                }
+            Console.ReadLine();
+        }
+        
     }
 }
