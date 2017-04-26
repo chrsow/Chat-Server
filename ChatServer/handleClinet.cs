7ed55f79222786a664236b68794221b27cddaeb2
5730539021-WS <liang_wasin@hotmail.co.th>
2017-04-26 21:03:59 +0700

    change buffer range to recieve stream

---
+++ ChatServer/handleClinet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    internal class handleClinet
+    {
+        internal void startClient(TcpClient clientSocket, string dataFromClient, Hashtable clientsList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
